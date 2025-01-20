adc9cf5b587131c2789543bea264feed7e10c6a6
David Liu <david-khala@hotmail.com>
2025-01-20 23:56:20 +0800

    add columns

--- format/tests/parquet_test.py
+++ format/tests/parquet_test.py
@@ -1,20 +1,25 @@
 import unittest
 
-from davidkhala.data.format.parquet import read
+from davidkhala.data.format.parquet import Parquet
 from pyarrow.parquet import ColumnSchema, ParquetSchema, ParquetLogicalType
 
+
 class MyTestCase(unittest.TestCase):
-    def test_inspect(self):
-        parquet_file = read('fixtures/gcp-data-davidkhala.dbt_davidkhala.country_codes.parquet')
-        schema: ParquetSchema = parquet_file.schema
-        self.assertIsInstance(schema, ParquetSchema)
-        for field in schema:
+    parquet = Parquet('fixtures/gcp-data-davidkhala.dbt_davidkhala.country_codes.parquet')
+
+    def test_schema(self):
+        self.assertIsInstance(self.parquet.schema, ParquetSchema)
+        for field in self.parquet.schema:
             self.assertIsInstance(field, ColumnSchema)
             print(field)
             self.assertIsInstance(field.logical_type, ParquetLogicalType)
             self.assertEqual(str(field.logical_type), "String")
-
-
+    def test_stream(self):
+        for micro_batch in self.parquet.read_stream():
+            print(micro_batch)
+            print('<<<<')
+            for column in micro_batch.column_names:
+                print(column, micro_batch.column(column))
 
 if __name__ == '__main__':
     unittest.main()
