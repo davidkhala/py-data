482232b61602fe88dae3b6cc593c857526f41aaa
David Liu <david-khala@hotmail.com>
2025-10-15 09:25:05 +0800

    0.0.9 update upsert

--- frame/tests/pandas_test.py
+++ frame/tests/pandas_test.py
@@ -15,20 +15,40 @@ class AirlineTestCase(unittest.TestCase):
         conn.register("airports", df_airports)
         conn.close()
 
+from davidkhala.data.frame.pandas import upsert
 class SyntaxTestCase(unittest.TestCase):
+
     def test_upsert(self):
-        from davidkhala.data.frame.pandas import upsert
-        prim_key ='id'
+
         df = pandas.DataFrame({
-            prim_key: [1, 2],
+            'id': [1, 2],
             'name': ['Alice', 'Bob'],
             'score': [85, 90]
-        }).set_index(prim_key)
+        })
+        prim_key ='id'
+        df = df.set_index(prim_key)
         new_record = {'name': 'Charlie', 'score': 95, prim_key:2}
-        upsert(df, prim_key, new_record)
+        upsert(df, prim_key, record = new_record)
 
         self.assertEqual(95, df.at[2, 'score'])
-
+    def test_upsert2(self):
+        df = pandas.DataFrame([
+            {'School': 'Oxford', 'Country': 'UK', 'Students': 1000},
+            {'School': 'Harvard', 'Country': 'US', 'Students': 1500}
+        ]).set_index(['School', 'Country'])
+        new_students = 2000
+        record = {'School': 'Oxford', 'Country': 'UK', 'Students': new_students}
+        df = upsert(df, 'School', 'Country', record=record)
+        self.assertEqual(new_students, df.loc[('Oxford', 'UK')].Students)
+    def test_upsert3(self):
+        df = pandas.DataFrame([
+            {'School': 'Oxford', 'Country': 'UK', 'Students': 1000},
+            {'School': 'Harvard', 'Country': 'US', 'Students': 1500}
+        ])
+        new_students = 2000
+        record = {'School': 'Oxford', 'Country': 'UK', 'Students': new_students}
+        df = upsert(df, 'School', 'Country', record=record)
+        self.assertEqual(new_students, df[(df['School'] == 'Oxford') & (df['Country'] == 'UK')].iloc[0].Students)
 
 
 
